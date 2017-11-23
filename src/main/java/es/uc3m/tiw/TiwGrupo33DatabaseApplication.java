c5d46bbab5ac6d3db25f14b72ef2833dd2b7be4f
FCLatorre <fernandoconde29@gmail.com>
2017-11-23 20:25:21 +0000

    Create the base Application class

---
+++ src/main/java/es/uc3m/tiw/TiwGrupo33DatabaseApplication.java
@@ -0,0 +1,12 @@
+package es.uc3m.tiw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiwGrupo33DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiwGrupo33DatabaseApplication.class, args);
+	}
+}
---
+++ src/test/java/es/uc3m/tiw/TiwGrupo33DatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package es.uc3m.tiw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TiwGrupo33DatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
