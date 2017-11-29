73991c20fcc55be9d4fb75dfc936e623f463f1c8
FCLatorre <fernandoconde29@gmail.com>
2017-11-29 15:26:02 +0000

    Include the Category entity

---
+++ src/main/java/es/uc3m/tiw/entities/Category.java
@@ -0,0 +1,62 @@
+package es.uc3m.tiw.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import java.util.List;
+
+
+/**
+ * The persistent class for the category database table.
+ * 
+ */
+@Entity
+@Table(name="category")
+@NamedQuery(name="Category.findAll", query="SELECT c FROM Category c")
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String name;
+
+	//bi-directional many-to-one association to Event
+	@OneToMany(mappedBy="categoryBean")
+	@JsonBackReference
+	private List<Event> events;
+
+	public Category() {
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Event> getEvents() {
+		return this.events;
+	}
+
+	public void setEvents(List<Event> events) {
+		this.events = events;
+	}
+
+	public Event addEvent(Event event) {
+		getEvents().add(event);
+		event.setCategoryBean(this);
+
+		return event;
+	}
+
+	public Event removeEvent(Event event) {
+		getEvents().remove(event);
+		event.setCategoryBean(null);
+
+		return event;
+	}
+
+}
